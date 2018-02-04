8cd45b7d745255fd7bad3771401dd2864db47254
Gideon Oduro <gideon.oduro@tele2.com>
2018-02-04 15:02:40 +0100

    fast facedetection i blur and face detection

--- src/main/java/se/bbs/api/FaceBlur.java
+++ src/main/java/se/bbs/api/FaceBlur.java
@@ -6,30 +6,23 @@ import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.springframework.stereotype.Component;
 import se.bbs.service.Camera;
-import se.bbs.service.ClassifierResolver;
 import se.bbs.service.Preview;
 import se.bbs.support.Rectangle;
 
-import java.util.List;
-
-import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_core.cvClearMemStorage;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
-import static org.bytedeco.javacpp.opencv_imgproc.CV_GAUSSIAN;
-import static org.bytedeco.javacpp.opencv_imgproc.cvSmooth;
 import static se.bbs.support.Image.changeColorSpace;
 import static se.bbs.support.Image.gray;
 
 @Component
 public class FaceBlur {
 
-    private ClassifierResolver classifierResolver;
     private Preview preview;
     private Rectangle rectangle;
     private FaceDetection faceDetection;
     private OpenCVFrameConverter.ToIplImage converter;
 
-    public FaceBlur(ClassifierResolver classifierResolver, Preview preview, Rectangle rectangle, FaceDetection faceDetection) {
-        this.classifierResolver = classifierResolver;
+    public FaceBlur(Preview preview, Rectangle rectangle, FaceDetection faceDetection) {
         this.preview = preview;
         this.rectangle = rectangle;
         this.faceDetection = faceDetection;
@@ -39,24 +32,19 @@ public class FaceBlur {
     public void preview(Camera source) throws FrameGrabber.Exception {
         OpenCVFrameGrabber camera = source.getWebCameSource();
         camera.start();
+        opencv_core.CvMemStorage storage = opencv_core.CvMemStorage.create();
+
         while (true) {
+            cvClearMemStorage(storage);
             opencv_core.IplImage image = converter.convert(camera.grab());
             opencv_core.IplImage gray = gray(image);
             changeColorSpace(image, gray, CV_BGR2GRAY);
-            opencv_core.CvMemStorage storage = opencv_core.CvMemStorage.create();
             opencv_core.CvSeq faces = faceDetection.detect(gray, storage, 1.1, 3);
-            System.out.println(faces.total());
-            if (faces.total() > 0) {
-                List<opencv_core.CvRect> rects = rectangle.getRect(faces);
-                rects.forEach(r -> {
-                    opencv_core.IplImage face = cvCreateImage(cvSize(r.width(), r.height()), image.depth(), image.nChannels());
-//                    cvSetImageROI(image, r);
-//                    cvCopy(image, face, null);
-                    cvSmooth(face, face, CV_GAUSSIAN, 51, 51, 0, 0);
-                });
-            }
+            rectangle.draw(faces, image);
             preview.show(converter.convert(image));
             cvClearMemStorage(storage);
+            preview.show(converter.convert(image));
+
         }
     }
 }
--- src/main/java/se/bbs/api/FaceDetection.java
+++ src/main/java/se/bbs/api/FaceDetection.java
@@ -14,41 +14,38 @@ import se.bbs.support.Rectangle;
 
 import static org.bytedeco.javacpp.opencv_core.cvClearMemStorage;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
-import static org.bytedeco.javacpp.opencv_objdetect.CV_HAAR_DO_CANNY_PRUNING;
-import static org.bytedeco.javacpp.opencv_objdetect.cvHaarDetectObjects;
+import static org.bytedeco.javacpp.opencv_objdetect.*;
 import static se.bbs.commons.Classifier.HAARCASCADE_FRONTALFACE_ALT;
 import static se.bbs.support.Image.changeColorSpace;
 import static se.bbs.support.Image.gray;
 
 @Component
 public class FaceDetection {
-    private ClassifierResolver classifierResolver;
     private Preview preview;
     private Rectangle rectangle;
     private OpenCVFrameConverter.ToIplImage converter;
+    private opencv_objdetect.CvHaarClassifierCascade cascade;
 
     @Autowired
     public FaceDetection(ClassifierResolver classifierResolver, Preview preview, Rectangle rectangle) {
-        this.classifierResolver = classifierResolver;
         this.preview = preview;
         this.rectangle = rectangle;
         this.converter = new OpenCVFrameConverter.ToIplImage();
+        this.cascade = new opencv_objdetect.CvHaarClassifierCascade(classifierResolver.get(HAARCASCADE_FRONTALFACE_ALT));
     }
 
     public opencv_core.CvSeq detect(opencv_core.CvArr image, opencv_core.CvMemStorage storage, double scaleFactor, int minNeighbors) {
-        opencv_objdetect.CvHaarClassifierCascade cascade =
-                new opencv_objdetect.CvHaarClassifierCascade(classifierResolver.get(HAARCASCADE_FRONTALFACE_ALT));
-        return cvHaarDetectObjects(image, cascade, storage, scaleFactor, minNeighbors, CV_HAAR_DO_CANNY_PRUNING);
+        return cvHaarDetectObjects(image, cascade, storage, scaleFactor, minNeighbors, CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH);
     }
 
     public void preview(Camera source) throws FrameGrabber.Exception {
         OpenCVFrameGrabber camera = source.getWebCameSource();
+        opencv_core.CvMemStorage storage = opencv_core.CvMemStorage.create();
         camera.start();
         while (true) {
             opencv_core.IplImage image = converter.convert(camera.grab());
             opencv_core.IplImage gray = gray(image);
             changeColorSpace(image, gray, CV_BGR2GRAY);
-            opencv_core.CvMemStorage storage = opencv_core.CvMemStorage.create();
             opencv_core.CvSeq faces = detect(gray, storage, 1.1, 3);
             rectangle.draw(faces, image);
             preview.show(converter.convert(image));
